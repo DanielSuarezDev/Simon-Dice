eee9e36439b199b21dcdfa3985d17ee88db2bdb7
Daniel Suarez Dev <danielsuarez3114@gmail.com>
2020-09-23 15:46:14 -0500

    juego Simon Dice totalmente responsivo

---
+++ main.js
@@ -0,0 +1,161 @@
+const btnEmpezar = document.getElementById("btnEmpezar");
+const celeste = document.getElementById("celeste");
+const violeta = document.getElementById("violeta");
+const naranja = document.getElementById("naranja");
+const verde = document.getElementById("verde");
+const ULTIMO_NIVEL = 10;
+
+/* Swal.fire({
+  title: "Bienvenidos al Juego",
+  showClass: {
+    popup: "animate__animated animate__fadeInDown"
+  },
+  hideClass: {
+    popup: "animate__animated animate__fadeOutUp"
+  }
+}); */
+
+class Juego {
+  constructor() {
+    this.inicializar = this.inicializar.bind(this);
+    this.inicializar();
+    this.generarSecuencia();
+    setTimeout(() => this.siguienteNivel(), 500);
+  }
+
+  inicializar() {
+    this.elegirColor = this.elegirColor.bind(this);
+    this.siguienteNivel = this.siguienteNivel.bind(this);
+    this.toggleBtnEmpezar();
+    this.nivel = 1;
+    this.colores = {
+      celeste,
+      violeta,
+      naranja,
+      verde
+    };
+  }
+  toggleBtnEmpezar() {
+    if (btnEmpezar.classList.contains("hide")) {
+      btnEmpezar.classList.remove("hide");
+    } else {
+      btnEmpezar.classList.add("hide");
+    }
+  }
+
+  generarSecuencia() {
+    this.secuencia = new Array(ULTIMO_NIVEL)
+      .fill(0)
+      .map(n => Math.floor(Math.random() * 4));
+  }
+
+  siguienteNivel() {
+    this.subNivel = 0;
+    this.iluminarSecuencia();
+    this.agregarEventoClick();
+  }
+
+  transformarNumeroAColor(numero) {
+    switch (numero) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
+    }
+  }
+
+  transformarColorAnumero(numero) {
+    switch (numero) {
+      case "celeste":
+        return 0;
+      case "violeta":
+        return 1;
+      case "naranja":
+        return 2;
+      case "verde":
+        return 3;
+    }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      const color = this.transformarNumeroAColor(this.secuencia[i]);
+      setTimeout(() => this.iluminarColor(color), 1000 * i);
+    }
+  }
+
+  iluminarColor(color) {
+    this.colores[color].classList.add("light");
+    setTimeout(() => this.apagarColor(color), 350);
+  }
+
+  apagarColor(color) {
+    this.colores[color].classList.remove("light");
+  }
+
+  agregarEventoClick() {
+    this.colores.celeste.addEventListener("click", this.elegirColor);
+    this.colores.verde.addEventListener("click", this.elegirColor);
+    this.colores.naranja.addEventListener("click", this.elegirColor);
+    this.colores.violeta.addEventListener("click", this.elegirColor);
+  }
+  eliminarEventosClick() {
+    this.colores.celeste.removeEventListener("click", this.elegirColor);
+    this.colores.verde.removeEventListener("click", this.elegirColor);
+    this.colores.naranja.removeEventListener("click", this.elegirColor);
+    this.colores.violeta.removeEventListener("click", this.elegirColor);
+  }
+
+  elegirColor(ev) {
+    console.log(ev);
+    const nombreColor = ev.target.dataset.color;
+    const numeroColor = this.transformarColorAnumero(nombreColor);
+    this.iluminarColor(nombreColor);
+    if (numeroColor === this.secuencia[this.subNivel]) {
+      this.subNivel++;
+      if (this.subNivel === this.nivel) {
+        this.nivel++;
+        this.eliminarEventosClick();
+        if (this.nivel === ULTIMO_NIVEL + 1) {
+          //gano
+          this.ganoEljuego();
+        } else {
+          setTimeout(this.siguienteNivel, 2000);
+        }
+      }
+    } else {
+      this.perdioEljuego();
+    }
+  }
+
+  ganoEljuego() {
+    Swal.fire({
+      title: "Que genial, Ganaste.",
+      width: 600,
+      padding: "3em",
+      background: "#fff",
+      backdrop: `rgba(0,0,123,0.4) url("images/Corazon.png") left top no-repeat`
+    });
+    this.inicializar();
+  }
+
+  perdioEljuego() {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Perdiste",
+      showConfirmButton: false,
+      timer: 1500
+    });
+    this.eliminarEventosClick();
+    this.inicializar();
+  }
+}
+
+function empezarJuego() {
+  const juego = new Juego();
+}
